a72a386c17efb61502929cd6f1a7dadb4405a1c0
ganondorofu <ganondorofu123@gmail.com>
2025-08-28 06:23:20 +0000

    firebaseの接続情報はコード内にある？

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBqEenr4S58Bd74rAoNCKbzltn0hI1FXT8",
-  authDomain: "print-logger.firebaseapp.com",
-  projectId: "print-logger",
-  storageBucket: "print-logger.appspot.com",
-  messagingSenderId: "17063338212",
-  appId: "1:17063338212:web:1d0465cb78bae47abeccf1",
-  measurementId: "G-FBJV780710"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
